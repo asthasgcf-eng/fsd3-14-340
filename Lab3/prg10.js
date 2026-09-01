b386a0a144a56493fb24b67bd1ebcbde2ed6781f
Astha Gupta <asthasgcf@gmail.com>
2026-09-01 10:37:05 +0530

    EchoAPI request testing

---
+++ Lab3/prg10.js
@@ -0,0 +1,23 @@
+import http from "http";
+import { createReadStream } from "fs";  
+const server = http.createServer((req, res) => {
+
+   if(req.url==="/" && req.method==="GET"){
+    res.end("<h1>Products details</h1>");
+}
+
+    else if(req.url==="/products" && req.method==="GET"){
+        res.writeHead(200, {"content-type": "text/json"});
+        const stream = createReadStream("./data/products.json",{
+            encoding: "utf-8"
+        });
+        stream.pipe(res);
+    }
+
+    else{
+        res.statusCode=404;
+        res.end("Not found");
+    }
+});
+
+server.listen(3000, () => console.log("Server is running at 3000..."));
