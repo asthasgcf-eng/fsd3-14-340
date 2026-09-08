5ab1689f4324c563658b92ca5c0390e043a9bf8c
Astha Gupta <asthasgcf@gmail.com>
2026-09-08 10:34:11 +0530

    Echo API POST(allteams)

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,10 +1,60 @@
-import http from"http";
+import http from "http";
+//import * as teams from 'teams.js';
 
-const server=http.createServer((req,res)=>{
+import { getAllTeams } from './teams.js';
 
-  res.end("<h1>SIH Internal</h1>");
+//import { parse as parseUrl } from "url";
+
+import { addTeam } from './teams.js';
+
+
+
+const PORT = 5000;
+
+const sendJson = (res, statusCode, data) => {
+  res.writeHead(statusCode, { "Content-Type": "application/json" });
+  res.end(data === "undefined" ? "Not Found" : JSON.stringify(data));
+};
+
+const parseJSONBody = (req) => {
+  return new Promise ((resolve, reject) => {
+    let body = "";
+    req.on("data", chunk => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      try {
+        resolve(body ? JSON.parse(body) : {});
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
+
+const server = http.createServer(async (req, res) => {
+ const url = new URL(req.url, `http://${req.headers.host}`);
+const pathname = url.pathname;
+const query = Object.fromEntries(url.searchParams);
+  const{ method } = req;
+  console.log("Pathname:", pathname, "query:", query, "Method:", method);
+
+  if(pathname === "/api/v1/teams" && method === "GET") {
+  let teams = getAllTeams();
+  return sendJson(res, 200, teams);
+
+}
+else if(pathname === "/api/v1/teams" && method === "POST") {
+  const newTeam = await parseJSONBody(req);
+  const addedTeam = addTeam(newTeam);
+  return sendJson(res, 201, addedTeam);
+}
+else{
+  res.statusCode = 404;
+}
+res.end();
 });
 
-server.listen(5000,()=>{
-    console.log("SIH Server is running");
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
